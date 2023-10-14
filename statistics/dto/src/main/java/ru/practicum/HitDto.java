417320953c377b535090f42a34088f84bbcfa1c3
Kirill Zykov <kompas009@yandex.ru>
2023-10-14 21:00:59 +0300

    Revert "Fix1: ФЗ v.1.0"
    This reverts commit 943d8de3

--- statistics/dto/src/main/java/ru/practicum/HitDto.java
+++ statistics/dto/src/main/java/ru/practicum/HitDto.java
@@ -1,6 +1,9 @@
 package ru.practicum;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -9,7 +12,10 @@ import lombok.*;
 @AllArgsConstructor
 public class HitDto {
 
-    private Long id;
     private String app;
     private String uri;
     private String ip;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
+}
